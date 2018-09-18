b343b7a392eb3f90cd26eb60565ce32dbf944bf7
Sali Zumberi <sali.zumberi@live.de>
2018-09-18 23:50:19 +0200

    bunch of cleanups

--- src/app/common/view/card-item/card-item.component.ts
+++ src/app/common/view/card-item/card-item.component.ts
@@ -2,117 +2,119 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import * as _ from 'lodash';
 import {DataService} from '../../services/data.service';
 import {map, share} from 'rxjs/operators';
-import {BehaviorSubject, combineLatest, forkJoin, Observable} from 'rxjs';
+import {combineLatest, Observable} from 'rxjs';
 import {Change} from '../../data/models/change';
+import {Diff} from '../../data/models/diff';
 
 @Component({
-  selector: 'card-item',
-  templateUrl: './card-item.component.html',
-  styleUrls: ['./card-item.component.css']
+    selector: 'card-item',
+    templateUrl: './card-item.component.html',
+    styleUrls: ['./card-item.component.css']
 })
 export class CardItemComponent implements OnInit {
-  @Input()
-  allData: any;
+    @Input()
+    allData: any;
 
-  @Input()
-  data: any;
+    @Input()
+    data: any;
 
-  @Input()
-  dataIndex: any;
+    @Input()
+    dataIndex: any;
 
-  @Input()
-  type: string;
+    @Input()
+    type: string;
 
-  @Input()
-  collapsedItems: number = 10;
+    @Input()
+    collapsedItems: number = 10;
 
-  @Output()
-  showDiffs = new EventEmitter();
+    @Output()
+    showDiffs = new EventEmitter();
 
-  @Input()
-  selectedChangeType;
+    @Input()
+    selectedChangeType;
 
-  show: boolean;
-  collapse: string;
+    show: boolean;
+    collapse: string;
 
-  pagedDiffItems: any;
-
-
-  diffs: any[];
-  diffsWhat: any[];
-  diffsWhere: any[];
-
-  isLoading: boolean = true;
+    pagedDiffItems: any;
 
 
-  diffExamples;
+    diffs: Diff[];
+    changes: Change[];
 
-  constructor(private dataService: DataService) {
-  }
+    isLoading: boolean = true;
 
-  ngOnInit() {
-    this.show = (this.dataIndex < this.collapsedItems);
-  }
 
-  public requestDataFromMultipleSources(): Observable<any[]> {
-    return combineLatest([
-      this.dataService.getDiffs(0, 1),
-      this.dataService.getDiffsWhat(0, 1),
-      this.dataService.getDiffsWhere(0, 1)]);
-  }
+    diffExamples;
 
-  toggleCollapse() {
-    this.isLoading = true;
-    this.show = !this.show;
-    this.collapse = this.collapse == 'open' ? 'closed' : 'open';
-    this.fetchDiffExamples();
-  }
-
-  getBackgroundGradient(percentage: string): string {
-    return 'rgba(220, 53, 69,' + percentage + ')';
-  }
-
-  getBackgroundGradientWord2vec(percentage: string): string {
-    return 'rgba(91, 192, 190,' + percentage + ')';
-  }
-
-  getSimilarDiffs(diff: any): Observable<any[]> {
-    return this.dataService.getDiffs(diff.changes.length - 1, diff.changes.length + 1).pipe(map(diffs => {
-      return diffs.filter(diffC => {
-        return _.isEqual(diff.changes.sort(), diffC.changes.sort());
-      });
-    }), share());
-  }
-
-  fetchDiffExamples() {
-    let diffs = [];
-    if (!this.isLoading) {
-      this.getDiffExamples();
-    } else {
-      this.requestDataFromMultipleSources().subscribe(([diffs, diffsWhat, diffsWhere]) => {
-        this.diffs = diffs;
-        this.diffsWhat = diffsWhat;
-        this.diffsWhere = diffsWhere;
-        this.isLoading = false;
-        this.getDiffExamples();
-      });
+    constructor(private dataService: DataService) {
     }
-  }
 
-  getDiffExamples() {
-    if (this.selectedChangeType == 'all') {
-      this.diffExamples = _.shuffle(this.diffs.filter(diff => {
-        return _.includes(diff.vectorized, this.data.index);
-      }));
-    } else if (this.selectedChangeType == 'what') {
-      this.diffExamples = _.shuffle(this.diffsWhat.filter(diff => {
-        return _.includes(diff.vectorized, this.data.index);
-      }));
-    } else if (this.selectedChangeType == 'where') {
-      this.diffExamples = _.shuffle(this.diffsWhere.filter(diff => {
-        let includus = _.includes(diff.vectorized, this.data.index);
-        return includus;
-      }));
+    ngOnInit() {
+        this.show = (this.dataIndex < this.collapsedItems);
     }
-  }
+
+    public requestDataFromMultipleSources(): Observable<any[]> {
+        return combineLatest([
+            this.dataService.getDiffs(0, 1), this.dataService.getChanges()]);
+    }
+
+    toggleCollapse() {
+        this.isLoading = true;
+        this.show = !this.show;
+        this.collapse = this.collapse === 'open' ? 'closed' : 'open';
+        this.fetchDiffExamples();
+    }
+
+    getBackgroundGradient(percentage: string): string {
+        return 'rgba(220, 53, 69,' + percentage + ')';
+    }
+
+    getBackgroundGradientWord2vec(percentage: string): string {
+        return 'rgba(91, 192, 190,' + percentage + ')';
+    }
+
+    getSimilarDiffs(diff: any): Observable<any[]> {
+        return this.dataService.getDiffs(diff.changes.length - 1, diff.changes.length + 1).pipe(map(diffs => {
+            return diffs.filter(diffC => {
+                return _.isEqual(diff.changes.sort(), diffC.changes.sort());
+            });
+        }), share());
+    }
+
+    fetchDiffExamples() {
+        if (!this.isLoading) {
+            this.getDiffExamples();
+        } else {
+            this.requestDataFromMultipleSources().subscribe(([diffsData, changes]) => {
+                this.diffs = diffsData;
+                this.changes = changes;
+                this.isLoading = false;
+                this.getDiffExamples();
+            });
+        }
+    }
+
+    getDiffExamples() {
+        if (this.selectedChangeType === 'all') {
+            this.diffExamples = _.shuffle(this.diffs.filter(diff => {
+                let includus = _.includes(diff.vectorized, this.data.index);
+                return includus;
+
+            }));
+        } else if (this.selectedChangeType === 'what') {
+            this.diffExamples = _.shuffle(this.diffs.filter(diff => {
+                let includus = _.includes(diff.vectorizedWhat, this.data.index);
+                return includus;
+
+            }));
+        } else if (this.selectedChangeType === 'where') {
+            this.diffExamples = _.shuffle(this.diffs.filter(diff => {
+                let includus = _.includes(diff.vectorizedWhere, this.data.index);
+                return includus;
+            }));
+        }
+
+
+    }
 }
